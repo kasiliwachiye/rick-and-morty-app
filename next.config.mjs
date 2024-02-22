12ada8867af0929d68adb42d015f2d78ca0512be
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-02-22 14:21:55 +0300

    chore: add rickmortyapi.com to the list of acceptible domains for image fetching

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "www.pexels.com",
+      "images.unsplash.com",
+      "127.0.0.1",
+      "rickandmortyapi.com"
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
