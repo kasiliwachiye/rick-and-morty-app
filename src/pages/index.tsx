928acdc9c1b2da18e3b1d8ee278971bac8fe3893
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-02-22 11:15:03 +0300

    chore: pick theme, configure app skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,56 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      Hello World
-    </main>
+    <>
+      <div className="navbar bg-primary">
+        <div className="flex-1">
+          <div className="btn btn-ghost text-xl">
+            <Image
+              src={"/rick-and-morty-logo.png"}
+              width={100}
+              height={50}
+              alt="rick-and-morty-logo"
+            />
+          </div>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="avatar"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section className="m-10">
+        <div className="flex justify-center">
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="text" className="grow" placeholder="Search" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </label>
+        </div>
+      </section>
+    </>
   );
 }
+
