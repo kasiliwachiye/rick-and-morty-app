fe0c8e93797230c17d98d198928e1b5e9f07205a
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-02-22 15:05:16 +0300

    chore: add hover to character name

--- src/components/character-card.tsx
+++ src/components/character-card.tsx
@@ -16,18 +16,18 @@ const CharacterCard = ({
   characterId,
 }: CardDetails) => {
   return (
-    <div className="card w-96 bg-white shadow-xl">
+    <div className="card w-80 bg-white shadow-xl">
       <div className="">
         <div className="rounded-t-lg h-24 overflow-hidden">
           <Image
-            width={50}
-            height={50}
+            width={80}
+            height={30}
             className="object-cover object-top w-full"
             src="https://images.unsplash.com/photo-1462332420958-a05d1e002413?q=80&w=2107&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="background"
           />
         </div>
-        <div className="mx-auto w-24 h-24 relative -mt-16 border-4 border-white rounded-full overflow-hidden">
+        <div className="mx-auto w-20 h-20 relative -mt-12 border-4 border-white rounded-full overflow-hidden">
           <Image
             width={50}
             height={50}
@@ -37,10 +37,10 @@ const CharacterCard = ({
           />
         </div>
         <div className="text-center mb-4">
-          <Link href={`/characters/${characterId}`} className="font-semibold">
+          <Link href={`/characters/${characterId}`} className="font-semibold hover:underline">
             {name}
           </Link>
-          <p className="text-gray-500 text-sm">{status}</p>
+          <p className="text-gray-500 text-xs">{status}</p>
         </div>
       </div>
     </div>
