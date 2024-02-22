f53fa136ad13ee6e01e16411d56f577e5ac03fbf
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-02-22 15:57:37 +0300

    chore: implement app head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
 import Navbar from "@/components/navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Wabalabadubdub</title>
+        <meta name="description" content="Wabalabadubdub" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <ToastContainer />
     </>
   );
 }
-
